3a86b882c4e0de73abdf7df3f9284d723c6b9602
notaSWE <iamnotaswe@gmail.com>
2022-12-02 18:37:18 -0800

    Housekeeping

--- pywrapped.py
+++ pywrapped.py
@@ -7,96 +7,17 @@ from ytmusicapi import YTMusic
 import json, random, requests, sys, time
 
 try:
-    filePath = sys.argv[1]  
+    # Load Google Takeout file as json
+    filePath = sys.argv[1]
+    with open(filePath) as ytHistory:
+        jsonHist = json.load(ytHistory)
 except:
     print("Usage: python3 pywrapped.py takeout/yourname.json")
     quit()
 
-# Initial config
-try:
-    wrFont = ImageFont.truetype("font/CircularStd-Book.otf", 46)
-    wrFontBold = ImageFont.truetype("font/CircularStd-Medium.otf", 54)
-except:
-    print("Remember to download/add fonts to font/ directory!")
-    quit()
-bg = Image.open("img/bg.png")
-imWidth = list(bg.size)[0]
-imHeight = list(bg.size)[1]
-printCoords = (100, (imHeight / 2) + 120)
-artistImgCoords = (270, 270)
-
-# Parse history and keep track of plays per artist
-with open(filePath) as ytHistory:
-    jsonHist = json.load(ytHistory)
-
-mostListenedTo = {}
-
-# Keep track of plays per song
-for item in jsonHist:
-    if item['header'] == 'YouTube Music':
-        metadata = item['subtitles']
-        metadata = metadata[0]
-        artist = metadata['name']
-        artist = artist.split(' - Topic')[0]
-        songName = item['title'].split("Watched ")[1]
-        if artist not in filteredArtists and artist in mostListenedTo:
-            # This value could get huge if you listen to the same song(s) an absurd amount of times; nested dictionary might be better
-            mostListenedTo[artist] += 1
-        elif artist not in filteredArtists and artist not in mostListenedTo:
-            mostListenedTo[artist] = 1
-
-# Get top 5 artists and add to top five list
-try:
-    topfive = {}
-    for k, _v in sorted(mostListenedTo.items(), key=lambda x: x[1], reverse=True)[:5]:
-        currArtist = k.split(' - Topic')[0]
-        if currArtist not in topfive.keys():
-            topfive[currArtist] = []
-except:
-    print("This script assumes you have at least 5 different artists in your YouTube Music history!")
-    quit()
-
-# Get the YouTube ChannelID of top artist
-topChannelId = None
-for item in jsonHist:
-    if item['header'] == 'YouTube Music':
-        metadata = item['subtitles']
-        metadata = metadata[0]
-        artist = metadata['name']
-        artist = artist.split(' - Topic')[0]
-        if artist == list(topfive.keys())[0]:
-            topChannelId = metadata['url'].split("/")[-1]
-            break
-
-# Inefficient but the values in topfive need to be filled with songs so...iterate through json again
-for item in jsonHist:
-    if item['header'] == 'YouTube Music':
-        metadata = item['subtitles']
-        metadata = metadata[0]
-        artist = metadata['name']
-        artist = artist.split(' - Topic')[0]
-        songName = item['title'].split("Watched ")[1]
-        if artist in topfive.keys():
-            topfive[artist].append(songName)
-
-# Get top songs
-topsongs = []
-for topartist in topfive.keys():
-    topartistSongs = topfive[topartist]
-    topsongs.append(max(set(topartistSongs), key=topartistSongs.count))
-
-# Function to draw top five artists in column one and corresponding songs in column two
-def draw_artists(printCoords):
-    for idx, val in enumerate(topfive):
-        toPrint = val
-        if len(toPrint) > 15:
-            toPrint = f"{toPrint[:12]}..."
-        draw_text.text(printCoords, f"{idx + 1} {toPrint}", font=wrFontBold, fill=(238, 248, 87))
-        printCoords = (printCoords[0], printCoords[1] + 80)
-
-# Function to draw songs in column two
-def draw_songs(printCoords):
-    for idx, val in enumerate(topsongs):
+# Function to draw a column (Top Artists, Top Songs)
+def draw_column(printCoords, colToDraw):
+    for idx, val in enumerate(colToDraw):
         toPrint = val
         if len(toPrint) > 15:
             toPrint = f"{toPrint[:12]}..."
@@ -111,17 +32,57 @@ def get_thumbnail(artistChannelString):
     response = requests.get(thumbs[0]['url'])
     return BytesIO(response.content)
 
+# Function to keep track of plays per song
+def parse_songs(pathToLoadedJson):
+    outDict = {}
+    for item in pathToLoadedJson:
+        if item['header'] == 'YouTube Music':
+            metadata = item['subtitles']
+            metadata = metadata[0]
+            artist = metadata['name']
+            artist = artist.split(' - Topic')[0]
+            songName = item['title'].split("Watched ")[1]
+            if artist not in filteredArtists and artist in outDict:
+                outDict[artist].append(str(songName))
+            elif artist not in filteredArtists and artist not in outDict:
+                outDict[artist] = [songName]
+    return top_five(outDict)
+
+# Get top 5 artists and add to topfive list
+def top_five(mostListenedToDict):
+    topfiveDict = {}
+    for k, v in sorted(mostListenedToDict.items(), key=lambda x: len(x[1]), reverse=True)[:5]:
+        currArtist = k.split(' - Topic')[0]
+        topfiveDict[currArtist] = max(set(list(v)), key=v.count)
+    return topfiveDict
+
+mostListenedTo = parse_songs(jsonHist)
+topfive = list(mostListenedTo.keys())
+topsongs = list(mostListenedTo.values())
+
+# Get the YouTube Music ChannelID of top artist
+topChannelId = None
+for item in jsonHist:
+    if item['header'] == 'YouTube Music':
+        metadata = item['subtitles']
+        metadata = metadata[0]
+        artist = metadata['name']
+        artist = artist.split(' - Topic')[0]
+        if artist == topfive[0]:
+            topChannelId = metadata['url'].split("/")[-1]
+            break
+
 # Draw Top Artists column
 draw_text = ImageDraw.Draw(bg)
-artistColCoords = (list(wrFont.getsize("Top Artists"))[0], list(wrFont.getsize("Top Artists"))[1])# list(supportFontLg.getsize(firstMain))[0]
+artistColCoords = (list(wrFont.getsize("Top Artists"))[0], list(wrFont.getsize("Top Artists"))[1])
 draw_text.text(printCoords, "Top Artists", font=wrFont, fill=(238, 248, 87))
 
 # Update printCoords to account for size of Top Artists text block
 printCoords = (printCoords[0], printCoords[1] + artistColCoords[1] + 40)
 
-draw_artists(printCoords)
+draw_column(printCoords, topfive)
 
-# Update printCoords to account for Top Songs starting position
+# Update printCoords to account for Top Songs start position
 printCoords = ((imWidth / 2) + 32, (imHeight / 2) + 120)
 
 songColCoords = (list(wrFont.getsize("Top Songs"))[0], list(wrFont.getsize("Top Artists"))[1])
@@ -130,7 +91,7 @@ draw_text.text(printCoords, "Top Songs", font=wrFont, fill=(238, 248, 87))
 # Update printCoords to account for size of Top Songs text block
 printCoords = (printCoords[0], printCoords[1] + artistColCoords[1] + 40)
 
-draw_songs(printCoords)
+draw_column(printCoords, topsongs)
 
 # Try to get and draw thumbnail in blank 540x540 box
 if topChannelId:
@@ -142,4 +103,5 @@ if topChannelId:
     except:
         pass
 
-bg.save(f"img/output_{int(time.time())}.png")
+# bg.save(f"img/output_{int(time.time())}.png")
+bg.save(f"img/output_dev.png")
--- settings.py
+++ settings.py
@@ -1,2 +1,20 @@
+from PIL import Image, ImageFont
+
+FONT_ONE = "font/CircularStd-Book.otf"
+FONT_TWO = "font/CircularStd-Medium.otf"
+
+try:
+    wrFont = ImageFont.truetype(FONT_ONE, 46)
+    wrFontBold = ImageFont.truetype(FONT_TWO, 54)
+except:
+    print("Remember to download/add fonts to font/ directory!")
+    quit()
+
+bg = Image.open("img/bg.png")
+imWidth = list(bg.size)[0]
+imHeight = list(bg.size)[1]
+printCoords = (100, (imHeight / 2) + 120)
+artistImgCoords = (270, 270)
+
 # Filtered artists
 filteredArtists = ['Original Soundtrack']
